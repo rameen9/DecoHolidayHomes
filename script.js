af4c22aeba6fd549db095d0c56afbd3183e7b0cb
rameen9 <rameenadeel12@gmail.com>
2024-12-01 21:47:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+    function toggleTheme() {
+        document.body.classList.toggle('dark-mode');
+    }
+</script>
+<button onclick="toggleTheme()">Toggle Dark Mode</button>
+<style>
+    .dark-mode {
+        background: #333;
+        color: #fff;
+    }
+</style>
